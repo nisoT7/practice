9e7a673154d3948a37f56059e9cdf2d3b17c947b
nisob64 <b6400114@planet.kanazawa-it.ac.jp>
2016-10-23 02:30:12 +0900

    2016_10_23_02:30:12

---
+++ git_upload.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#all file upload
+git add -A
+
+if [ $# != 0 ]; then
+    git commit -m "$1 `date '+%Y_%m_%d_%H:%M:%S'`"
+else
+    git commit -m "`date '+%Y_%m_%d_%H:%M:%S'`"
+fi
+
+git push -u origin master
